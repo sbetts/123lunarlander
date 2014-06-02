d1c19a931719e3956f054d120f3521053c47f13f
Scott Betts <wetheliving@gmail.com>
2014-06-02 04:34:22 -0700

    Cleaned up

---
+++ lunarlanderclient.py
@@ -0,0 +1,271 @@
+## Lunar Lander Multiplayer Client
+## Scott Betts; 76651961
+
+
+
+import pygame, sys, random
+from network import Handler, poll
+from threading import Thread
+from time import sleep
+
+from pygame.locals import *
+
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+
+
+
+windowSurfaceObj = pygame.display.set_mode((640,480))
+pygame.display.set_caption('Lunar Lander')
+landerSurfaceObj = pygame.image.load('playerlander2.png')
+blackColor = pygame.Color(0, 0, 0)
+greyColor = pygame.Color(215, 215, 215)
+gameWinLoseText = pygame.Surface((0, 0))
+gameStartScreenText = pygame.Surface((0, 0))
+startFont = pygame.font.Font(None, 26)
+startFont2 = pygame.font.Font(None, 22)
+gameMenuText1 = pygame.Surface((0, 0))
+gameMenuText2 = pygame.Surface((0, 0))
+gameMenuText3 = pygame.Surface((0, 0))
+gameSelectorText1 = pygame.Surface((0, 0))
+gameSelectorText2 = pygame.Surface((0, 0))
+gameInstructionText1 = pygame.Surface((0, 0))
+gameInstructionText2 = pygame.Surface((0, 0))
+gameFont = pygame.font.Font(None, 18)
+landerYVelocityText = pygame.Surface((0, 0))
+landerPlayerIdentifierText = pygame.Surface((0, 0))
+
+
+
+
+landerList = []
+particleList = []
+
+
+keyIsDown = False
+gameOver = False
+should_exit = False
+downKey = 0
+
+pygame.key.set_repeat(33, 33)
+
+
+
+groundPoints = [(0, 400), (50, 400), (50, 450), (115, 450), (115, 350), (250, 350), (250, 410), (260, 410),
+                (260, 450), (330, 450), (330, 320), (345, 320), (345, 375), (500, 375), (500, 360), (600, 360),
+                (600, 460), (640, 460)]
+
+
+
+groundRectList = [pygame.Rect(0, 400, 50, 80), pygame.Rect(50, 450, 65, 30), pygame.Rect(115, 350, 135, 130),
+                  pygame.Rect(250, 410, 10, 70), pygame.Rect(260, 450, 70, 30), pygame.Rect(330, 320, 15, 160),
+                  pygame.Rect(345, 375, 155, 105), pygame.Rect(500, 360, 100, 120), pygame.Rect(600, 460, 40, 20)]
+
+
+
+class Client(Handler):
+    
+    def on_close(self):
+        global should_exit
+        should_exit = True
+        
+    def on_msg(self, msg):
+        if msg.has_key('playerid'):
+            self._playerID = msg['playerid']
+        else:
+            global landerList
+            global particleList
+            landerList = msg['landerList']
+            particleList = msg['particleList']
+
+
+    def getPlayerID(self):
+        return self._playerID
+
+
+
+def checkForKeyPress():
+    downKey = 0
+    if pygame.event.peek(KEYDOWN) == False:
+        keyIsDown = False
+
+    for event in pygame.event.get():
+        downKey = 0
+        if gameOver == True:
+            pygame.quit()
+            sys.exit()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN:
+            keyIsDown = True
+            if event.key == K_UP:
+                downKey = 1 # 1 is up arrow.
+            elif event.key == K_LEFT:
+                downKey = 2 # 2 is left arrow.
+            elif event.key == K_RIGHT:
+                downKey = 3 # 3 is right arrow.
+            elif event.key == K_ESCAPE:
+                downKey = 4 # 4 is a dummy value.
+                pygame.event.post(pygame.event.Event(QUIT))
+
+    return downKey
+
+
+        
+def checkForMenuKeyPress():
+    downKey = 0
+    if pygame.event.peek(KEYDOWN) == False:
+        keyIsDown = False
+
+    for event in pygame.event.get():
+        downKey = 0
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN:
+            keyIsDown = True
+            if event.key == K_UP:
+                downKey = 1 # 1 is up arrow.
+            elif event.key == K_DOWN:
+                downKey = 2 # 2 is down arrow.
+            elif event.key == K_RETURN:
+                downKey = 3 # 3 is return.
+            elif event.key == K_ESCAPE:
+                downKey = 4 # 4 is a dummy value.
+                pygame.event.post(pygame.event.Event(QUIT))
+
+    return downKey
+
+
+
+def sendCommandToServer(client, command):
+    client.do_send({'command': command})
+
+
+
+def beginGame():
+
+    host, port = '192.168.0.3', 8888
+    client = Client(host, port)
+
+    has_lander = False
+    game_over = False
+
+    game_over_tick = 0
+
+
+    while game_over_tick < 120:
+
+        if game_over == True:
+            game_over_tick += 1
+            
+        if should_exit == True:
+            ##print('**** Disconnected from server ****')
+            ##sys.exit()
+            pass
+
+        poll()
+        
+        windowSurfaceObj.fill(blackColor)
+
+
+        pygame.draw.lines(windowSurfaceObj, greyColor, False, groundPoints)
+        
+        for x in particleList:
+            pygame.draw.rect(windowSurfaceObj, greyColor, pygame.Rect(x[0], x[1], 2, 2))
+
+        
+        for x in landerList:
+            if x[7] != 'DESTROYED':
+                windowSurfaceObj.blit(pygame.transform.rotate(landerSurfaceObj, x[0]), (x[1], x[2]))
+
+        for x in landerList:
+            try:
+                if x[4] == client.getPlayerID():
+                    has_lander = True
+                    myLander = x
+            except:
+                pass
+
+        if has_lander:
+                if myLander[5] != '':
+                    game_over = True
+                    gameWinLoseText = gameFont.render(myLander[5], True, greyColor)
+
+        if has_lander:
+            landerPlayerIdentifierText = gameFont.render('You', True, greyColor)
+            windowSurfaceObj.blit(landerPlayerIdentifierText, (myLander[1], myLander[2] - 20))
+            landerFuelText = gameFont.render('Lander Y Velocity: ' + str(myLander[6]), True, greyColor)
+            windowSurfaceObj.blit(landerFuelText, (400, 30))
+
+        if game_over:
+            windowSurfaceObj.blit(gameWinLoseText, (30, 30))
+
+        if has_lander:
+            landerFuelText = gameFont.render('Lander Fuel: ' + str(myLander[3]), True, greyColor)
+            windowSurfaceObj.blit(landerFuelText, (400, 50))
+
+        userCommand = checkForKeyPress()
+
+        sendCommandToServer(client, userCommand)
+
+        
+        pygame.display.update()
+        fpsClock.tick(30)
+
+    client.do_close()
+
+
+
+def startScreen():
+    exit_game = False
+    player_selection = 1
+    while exit_game == False:
+        windowSurfaceObj.fill(blackColor)
+        gameStartScreenText = startFont.render('LUNAR LANDER', True, greyColor)
+        windowSurfaceObj.blit(gameStartScreenText, (150, 115))
+        windowSurfaceObj.blit(landerSurfaceObj, (100, 115))
+        gameMenuText1 = startFont2.render('MAIN MENU', True, greyColor)
+        windowSurfaceObj.blit(gameMenuText1, (150, 145))
+        gameMenuText2 = gameFont.render('START GAME', True, greyColor)
+        windowSurfaceObj.blit(gameMenuText2, (150, 175))
+        gameMenuText3 = gameFont.render('EXIT', True, greyColor)
+        windowSurfaceObj.blit(gameMenuText3, (150, 190))
+        gameInstructionText1 = gameFont.render('Up arrow key to fire thrusters, left and right arrow keys to rotate.', True, greyColor)
+        gameInstructionText2 = gameFont.render('Land flat on the moon, but make it a soft landing!', True, greyColor)
+        windowSurfaceObj.blit(gameInstructionText1, (200, 270))
+        windowSurfaceObj.blit(gameInstructionText2, (200, 290))
+        if player_selection == 1:
+            gameSelectorText1 = gameFont.render('>>>', True, greyColor)
+            windowSurfaceObj.blit(gameSelectorText1, (110, 173))
+            gameSelectorText2 = gameFont.render('<<<', True, greyColor)
+            windowSurfaceObj.blit(gameSelectorText2, (250, 173))
+        elif player_selection == 2:
+            gameSelectorText1 = gameFont.render('>>>', True, greyColor)
+            windowSurfaceObj.blit(gameSelectorText1, (110, 188))
+            gameSelectorText2 = gameFont.render('<<<', True, greyColor)
+            windowSurfaceObj.blit(gameSelectorText2, (250, 188))
+
+        userMenuCommand = checkForMenuKeyPress()
+
+        if userMenuCommand == 1 or userMenuCommand == 2:
+            if player_selection == 1:
+                player_selection = 2
+            else:
+                player_selection = 1
+        if userMenuCommand == 3:
+            if player_selection == 1:
+                beginGame()
+            else:
+                pygame.quit()
+                sys.exit()
+            
+        pygame.display.update()
+        fpsClock.tick(30)
+            
+startScreen()
+
+
---
+++ lunarlanderserver.py
@@ -0,0 +1,363 @@
+## Lunar Lander Multiplayer Server
+## Scott Betts; 76651961
+
+
+import pygame, sys, random
+
+from pygame.locals import *
+from network import Listener, Handler, poll
+
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+
+
+
+windowSurfaceObj = pygame.display.set_mode((640,480))
+pygame.display.set_caption('Lunar Lander')
+landerSurfaceObj = pygame.image.load('playerlander2.png')
+blackColor = pygame.Color(0, 0, 0)
+greyColor = pygame.Color(215, 215, 215)
+gameWinLoseText = pygame.Surface((0, 0))
+gameFont = pygame.font.Font(None, 18)
+
+
+
+GRAVITY_CONSTANT = 0.0004
+STARTING_LANDER_FUEL = 225
+
+
+handlerList = {}
+playerIDCounter = 1
+landerList = {}
+particleList = []
+
+
+groundPoints = [(0, 400), (50, 400), (50, 450), (115, 450), (115, 350), (250, 350), (250, 410), (260, 410),
+                (260, 450), (330, 450), (330, 320), (345, 320), (345, 375), (500, 375), (500, 360), (600, 360),
+                (600, 460), (640, 460)]
+
+
+groundRectList = [pygame.Rect(0, 400, 50, 80), pygame.Rect(50, 450, 65, 30), pygame.Rect(115, 350, 135, 130),
+                  pygame.Rect(250, 410, 10, 70), pygame.Rect(260, 450, 70, 30), pygame.Rect(330, 320, 15, 160),
+                  pygame.Rect(345, 375, 155, 105), pygame.Rect(500, 360, 100, 120), pygame.Rect(600, 460, 40, 20)]
+
+
+startingPointList = [320, 280, 360, 400, 240, 200, 440, 160, 480, 120, 520, 80, 560]
+startingPointIterator = 0
+
+
+tooFarRightMessage = 'You lose: lander left the playing field.'
+tooFarLeftMessage = 'You lose: lander left the playing field.'
+tooFastMessage = 'You lose: lander traveling too fast.'
+tooSkewedMessage = 'You lose: lander crashed at an angle.'
+unevenTerrainMessage = 'You lose: landing point too small.'
+landersCollidedMessage = 'You lose: lander collided with another lander!'
+
+
+
+
+
+class MyHandler(Handler):
+     
+    def on_open(self):
+        global playerIDCounter
+        handlerList[playerIDCounter] = self
+        playerIDCounter += 1
+        self.do_send({'playerid':playerIDCounter})
+        landerList[self] = Lander(generateStartingPosX(), 80, 0, 0, 0, STARTING_LANDER_FUEL, playerIDCounter)
+        ##landerList[self] = Lander(320, 80, 0, 0, 0, STARTING_LANDER_FUEL)
+        pass
+         
+    def on_close(self):
+        pass
+     
+    def on_msg(self, msg):
+        if msg.has_key('command'):
+            landerList[self].setCommand(msg['command'])
+
+
+class Lander:
+
+
+    def __init__(self, startingPosX, startingPosY, startingVelocityX, startingVelocityY, startingAngle, startingFuel, playerID):
+        self._posX = startingPosX
+        self._posY = startingPosY
+        self._velocityX = startingVelocityX
+        self._velocityY = startingVelocityY
+        self._angle = startingAngle
+        self._fuel = startingFuel
+        self._xAcceleration = 0
+        self._yAcceleration = 0
+        self._landerRect = windowSurfaceObj.blit(landerSurfaceObj, (self._posX, self._posY))
+        self._shouldMove = True
+        self._status = 'IN_PLAY'
+        self._playerID = playerID
+        self._lossMessage = ''
+        self._command = 0
+
+
+    def getPosX(self):
+        return self._posX
+
+
+    def getPosY(self):
+        return self._posY
+
+
+    def getVelocityX(self):
+        return self._velocityX
+
+
+    def getVelocityY(self):
+        return self._velocityY
+
+
+    def getPlayerID(self):
+        return self._playerID
+
+
+    def getAngle(self):
+        return self._angle
+
+
+    def getFuel(self):
+        return self._fuel
+
+
+    def getRect(self):
+        return self._landerRect
+
+
+    def setMoving(self, shouldMove):
+        self._shouldMove = shouldMove
+
+
+    def getStatus(self):
+        return self._status
+
+
+    def setStatus(self, status):
+        self._status = status
+
+    def getPlayerID(self):
+        return self._playerID
+
+
+    def setCommand(self, command):
+        self._command = command
+
+
+    def setLossMessage(self, lossMessage):
+        self._lossMessage = lossMessage
+
+    def getLossMessage(self):
+        return self._lossMessage
+
+
+    def update(self):
+        if self._shouldMove == True:
+            if self._command == 1:
+                if self._angle >= 0 and self._angle <= 90 and self._fuel > 0:
+                    xValue = self._angle / 90.0
+                    self._velocityY -= (1 - xValue) * 0.15
+                    self._velocityX += 0.3 * -xValue
+                    self._yAcceleration = 0
+                    self._fuel = self._fuel - 1
+                    for x in range(1):
+                        particleList.append(PropulsionParticle(self._posX, self._posY, self._velocityX, self._velocityY))
+                if self._angle < 0 and self._angle >= -90 and self._fuel > 0:
+                    xValue = self._angle / 90.0
+                    self._velocityY -= (1 - -xValue) * 0.15
+                    self._velocityX += 0.3 * -xValue
+                    self._yAcceleration = 0
+                    self._fuel = self._fuel - 1
+                    for x in range(1):
+                        particleList.append(PropulsionParticle(self._posX, self._posY, self._velocityX, self._velocityY))
+                self._command = 0
+            if self._command == 2:
+                self._angle += 5
+                if self._angle > 90:
+                    self._angle = 90
+                self._command = 0
+            if self._command == 3:
+                self._angle -= 5
+                if self._angle < -90:
+                    self._angle = -90
+                self._command = 0
+            self._yAcceleration += GRAVITY_CONSTANT
+            self._velocityY += self._yAcceleration
+            self._posY += self._velocityY
+            self._posX += self._velocityX
+        if self._status != 'DESTROYED':
+            self._landerRect = windowSurfaceObj.blit(pygame.transform.rotate(landerSurfaceObj, self._angle), (self._posX, self._posY))
+
+
+
+class PropulsionParticle:
+
+
+    def __init__(self, startingPosX, startingPosY, startingVelocityX, startingVelocityY):
+        self._posX = startingPosX + 10
+        self._posY = startingPosY + 20
+        self._velocityX = -startingVelocityX * 4 * random.random() + random.random() - random.random()
+        self._velocityY = -startingVelocityY + 3
+        self._lifetime = 30 + (30 * random.random())
+        self._isAlive = True
+
+
+    def update(self):
+        if self._lifetime <= 0:
+            self._isAlive = False
+        self._posX = self._posX + self._velocityX
+        self._posY = self._posY + self._velocityY
+        self._lifetime = self._lifetime - 1
+
+
+    def queuedForRemoval(self):
+        return not self._isAlive
+
+
+    def getPosX(self):
+        return self._posX
+
+    def getPosY(self):
+        return self._posY
+
+
+def checkForCollision(lander1, lander2):
+    if lander1.getRect().colliderect(lander2.getRect()) == True:
+        if lander1.getStatus() == 'IN_PLAY' and lander2.getStatus() == 'IN_PLAY':
+            playerLoses(lander1, landersCollidedMessage)
+            playerLoses(lander2, landersCollidedMessage)
+            return True
+
+
+
+def checkForLanding(lander, landerLoc):
+    if lander.getRect().centerx > 640:
+        playerLoses(lander, tooFarRightMessage)
+        return True
+    elif lander.getRect().right < 5:
+        playerLoses(lander, tooFarLeftMessage)
+        return True
+    elif lander.getRect().collidelist(groundRectList) != -1:
+        if lander.getVelocityY() >= 1:
+            playerLoses(lander, tooFastMessage)
+            return True
+        elif lander.getAngle() > 5:
+            playerLoses(lander, tooSkewedMessage)
+            return True
+        elif lander.getAngle() < -5:
+            playerLoses(lander, tooSkewedMessage)
+            return True
+        else:
+            if groundRectList[lander.getRect().collidelist(groundRectList)].collidepoint(lander.getRect().bottomleft):
+                if groundRectList[lander.getRect().collidelist(groundRectList)].collidepoint(lander.getRect().bottomright):
+                    lander.setLossMessage('YOU WIN!')
+                    lander.setMoving(False)
+                    lander.setStatus('WINNER')
+                else:
+                    playerLoses(lander, unevenTerrainMessage)
+                    return True
+            else:
+                playerLoses(lander, unevenTerrainMessage)
+                return True
+
+
+def playerLoses(lander, lossMessage):
+    if lander.getStatus() != 'WINNER':
+        lander.setLossMessage(lossMessage)
+
+
+def destroyLander(landerLoc):
+##    for x in range(36):
+##        particleList.append(PropulsionParticle(landerList[landerLoc].getPosX(), landerList[landerLoc].getPosY(), 0, 2))
+##        particleList.append(PropulsionParticle(landerList[landerLoc].getPosX() + 5, landerList[landerLoc].getPosY() - 3, 0, 2))
+##        particleList.append(PropulsionParticle(landerList[landerLoc].getPosX() - 5, landerList[landerLoc].getPosY() - 3, 0, 2))
+    landerList[landerLoc].setStatus('DESTROYED')
+    landerList[landerLoc].setMoving(False)
+    ##del landerList[landerLoc]
+    print('Successfully destroyed lander.')
+    if len(landerList) == 0:
+        pass
+        ##SERVER GAME OVER GOES HERE
+
+
+def generateStartingPosX():
+    global startingPointIterator
+    thisValue = startingPointList[startingPointIterator]
+    if startingPointIterator > 11:
+        startingPointIterator = 0
+    else:
+        startingPointIterator += 1
+    return thisValue
+
+
+
+def generateGameStateForClients(thisLanderList, thisParticleList):
+    landerListForClients = []
+    particleListForClients = []
+    gameState = {}
+    for x in thisLanderList:
+        landerListForClients.append([thisLanderList[x].getAngle(), thisLanderList[x].getPosX(), thisLanderList[x].getPosY(),
+                                     thisLanderList[x].getFuel(), thisLanderList[x].getPlayerID(), thisLanderList[x].getLossMessage(),
+                                     thisLanderList[x].getVelocityY(), thisLanderList[x].getStatus()])
+    gameState['landerList'] = landerListForClients
+    for x in thisParticleList:
+        particleListForClients.append([x.getPosX(), x.getPosY()])
+    gameState['particleList'] = particleListForClients
+    return gameState
+
+
+def sendGameStateToClients(gameState):
+    for x in handlerList:
+        handlerList[x].do_send(gameState)
+
+port = 8888
+server = Listener(port, MyHandler)
+
+while True:
+    windowSurfaceObj.fill(blackColor)
+
+
+    pygame.draw.lines(windowSurfaceObj, greyColor, False, groundPoints)
+
+    poll(timeout=0.05)
+
+    for x in particleList:
+        pygame.draw.rect(windowSurfaceObj, greyColor, pygame.Rect(x.getPosX(), x.getPosY(), 2, 2))
+
+
+    
+
+
+
+
+    
+    for x in landerList:
+        landerList[x].update()
+    for x in particleList:
+        x.update()
+        if x.queuedForRemoval():
+            particleList.remove(x)
+    landersToBeDestroyed = set()
+    for x in landerList:
+        for y in landerList:
+            if landerList[x] != landerList[y]:
+                if checkForCollision(landerList[x], landerList[y]):
+                    landersToBeDestroyed.add(x)
+                    landersToBeDestroyed.add(y)
+    for x in landerList:
+        if checkForLanding(landerList[x], x):
+            if landerList[x].getStatus() == 'IN_PLAY':
+                landersToBeDestroyed.add(x)
+    currentGameStateForClients = generateGameStateForClients(landerList, particleList)
+    sendGameStateToClients(currentGameStateForClients)
+    for x in landersToBeDestroyed:
+        destroyLander(x)
+    
+    
+    ##pygame.display.update()
+    fpsClock.tick(30)
